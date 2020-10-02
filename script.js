f0e2c106c939e5d71bdf09a468b2520286a4b399
Kazybe1998 <andasbek.kazybek@gmail.com>
2020-10-02 08:42:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(typeof(answer));
+// console.log(answer+1);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваше фамилия?", "");
+// answers[2] = prompt("Скольео вам лет?", "");
+
+// document.write(answers);
+// console.log(answers)
+
+const  category  = "toys";
+
+console.log("https://someurl.com/${cotegory}/5");
+
+const user = "Kazybek";
+
+alert(`Hello, ${user}`);
